56b9d5b48c86065f1877f90267f3fe392a2179dd
IuryRezende <iuryrezende5098@gmail.com>
2025-05-15 18:34:27 -0300

    Validation function refactored

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
 const sudoku = document.querySelector(".sudoku");
+let AllRows;
+let AllColumns;
+
 function random()
 {
     return Math.trunc(Math.random() * 9) + 1;
 }
 
-function validNumberAxisY(AllRows, currentInput, columnIndex, value)
+function isColumnValid(currentRow, currentColumn)
 {
+    const currentInput = currentRow.querySelectorAll("input")[currentColumn];
+
     for (let i = 0; i < AllRows.length; i++)
     {
-        const input = AllRows[i].querySelectorAll("input")[columnIndex];
-        if (input !== currentInput[columnIndex] && input.value === value)
+        const input = AllRows[i].querySelectorAll("input")[currentColumn];
+        if (input !== currentInput && input.value === currentInput.value)
         {
             return false;
         }
@@ -17,34 +22,33 @@ function validNumberAxisY(AllRows, currentInput, columnIndex, value)
     return true;
 }
 
-function validNumberAxisX(Allcolumns, currentRow, columnIndex, value)
+function isRowValid(currentRow, currentColumn)
 {
-    for (let i = 0; i < Allcolumns.length; i++)
+    const currentInput = currentRow.querySelectorAll("input")[currentColumn];
+    for (let i = 0; i < AllColumns.length; i++)
     {
         const input = currentRow.querySelectorAll("input")[i];
-        const currentInput = currentRow.querySelectorAll("input")[columnIndex];
-        if (input !== currentInput && input.value === value)
+        if (input !== currentInput && input.value === currentInput.value)
         {
             return false;    
         } 
-
+            
     }
     return true;
 }
 
-function validNumberBlock(AllRows, atualCell, rowIndex, columnIndex){
-    const startColumn = Math.floor(columnIndex/3) * 3;
-    const startRow = Math.floor(rowIndex/3) * 3;
+function isBlockValid(atualCell, rowIndex, columnIndex){
+    const startRow = Math.trunc(rowIndex/3) * 3;
+    const startColumn = Math.trunc(columnIndex/3) * 3;
     const cell = atualCell.querySelectorAll("input")[columnIndex];
     
-    for(let i = startColumn; i < (startColumn + 3); i++)
+    for(let i = startRow; i < (startRow + 3); i++)
     {
-        for (let j = startRow; j < (startRow + 3); j++)
+        for (let j = startColumn; j < (startColumn + 3); j++)
         {
             const input = AllRows[i].querySelectorAll("input")[j];
             if (input !== cell && input.value === cell.value)
             {
-                
                 return false;
             }
         }
@@ -52,25 +56,33 @@ function validNumberBlock(AllRows, atualCell, rowIndex, columnIndex){
     return true;
 }
 
+function isAllValid(currentRow, rowIndex, colIndex)
+{
+    return isColumnValid(currentRow, colIndex) &&
+           isRowValid(currentRow, colIndex) &&
+           isBlockValid(currentRow, rowIndex, colIndex); 
+}
+    
 document.getElementById("start").addEventListener("click", () =>{
     sudoku.innerHTML = "";
     for (let i = 0; i < 9; i++)
-    {
+        {
         const row = document.createElement("tr");
         if (i == 2 || i == 5)
         {
-            row.style.borderBottom = "4px solid black";
+            row.style.borderBottom = "3px solid black";
         }
         for (let j = 0 ; j < 9; j++)
         {
             const numberInput = document.createElement("input");
+            numberInput.inputMode = "numeric";
             numberInput.maxLength = 1;
-
+        
             const cell = document.createElement("td");
-
+        
             if (j == 2 || j == 5)
             {
-                cell.style.borderRight = "4px solid black";
+                cell.style.borderRight = "3px solid black";
             }
             
             cell.appendChild(numberInput);
@@ -78,26 +90,37 @@ document.getElementById("start").addEventListener("click", () =>{
         }
         sudoku.appendChild(row);
     }
+    AllRows = document.querySelectorAll(".sudoku tr");
+    
+    AllRows.forEach((currentRow, rowIndex) => {
+        AllColumns = currentRow.querySelectorAll("input");
 
-    const rows = document.querySelectorAll(".sudoku tr");
-
-    rows.forEach((row, rowIndex) => {
-        const inputs = row.querySelectorAll("input");
-
-        inputs.forEach((input, colIndex) => {
+        AllColumns.forEach((input, colIndex) => {
             const luckyBox = random();
-            if (luckyBox <= 1)
+            if (luckyBox <= 2)
             {
                 input.value = random();
-                while(!validNumberAxisY(rows, inputs, colIndex, input.value) || !validNumberAxisX(inputs, row, colIndex, input.value) || !validNumberBlock(rows, row, rowIndex, colIndex))
+
+                while(!isAllValid(currentRow, rowIndex, colIndex))
                 {
                     input.value = random();
                 }
-                input.style.backgroundColor = "rgba(136, 136, 136, 0.4)";
+                
+                input.style.backgroundColor = "rgba(136, 136, 136, 0.5)";
+                input.style.pointerEvents = "none";
             }
             else{
                 input.value = null;
             }
         })
     })
+
 })
+
+// document.addEventListener("keydown", (e) => {
+//     const isLetter = /^[a-z]/g.test(e.key);
+//     if (isLetter)
+//         {
+//         console.log(userCells);
+//     }
+// })
